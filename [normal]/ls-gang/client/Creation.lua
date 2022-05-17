3315be3b463bfbec71aa614ebfd4127a579dd686
kyeonghoon <zhaqj12@naver.com>
2022-05-17 09:51:17 +0900

    warMenu Creation 01.

--- [normal]/ls-gang/client/Creation.lua
+++ [normal]/ls-gang/client/Creation.lua
@@ -50,6 +50,11 @@ WarMenu.CreateSubMenu('demo_points', 'demo', _U('points'))
 WarMenu.CreateSubMenu('confirm', 'demo', _U('conf'))
 WarMenu.CreateSubMenu('demo_exit', 'demo', _U('sure'))
 
+RegisterNetEvent('ls-gang:Cgettypes')
+AddEventHandler('ls-gang:Cgettypes',function (type)
+  return type;
+end)
+
 RegisterNetEvent('ls-gang:client:openCreation')
 AddEventHandler('ls-gang:client:openCreation', function()
 
@@ -60,7 +65,7 @@ AddEventHandler('ls-gang:client:openCreation', function()
     WarMenu.OpenMenu('demo')
     while true do
       if WarMenu.Begin('demo') then
-        WarMenu.MenuButton('Gang Info', 'demo_menu')
+        WarMenu.MenuButton('팩션 정보', 'demo_menu')
         WarMenu.MenuButton('Gang style', 'demo_style')
         WarMenu.MenuButton('Gang vehicles', 'demo_vehicles')
         WarMenu.MenuButton('Add points', 'demo_points')
@@ -68,6 +73,77 @@ AddEventHandler('ls-gang:client:openCreation', function()
         WarMenu.MenuButton('Exit', 'demo_exit')
 
         WarMenu.End() 
+      elseif WarMenu.Begin('demo_menu') then
+        if gangName == nil then
+          local pressed, inputText = WarMenu.InputButton('Gang name:', nil, _inputText)
+          if pressed then
+              if inputText then
+                  gangName = inputText
+              end
+          end
+        else
+          local pressed, inputText = WarMenu.InputButton('Gang name: ~r~' ..gangName, nil, _inputText)
+                if pressed then
+                    if inputText then
+                        gangName = inputText
+                    end
+                end
+        end
+
+        if maxMembers == nil then
+          local pressed, inputText = WarMenu.InputButton('Max members:', nil, _inputText)
+          if pressed then
+              if inputText then
+                  local maxOnes = tonumber(inputText)
+                  if maxOnes then
+                      maxMembers = maxOnes
+                  else
+                      ESX.ShowNotification('Max members must be a number')
+                  end
+              end
+          end
+        else
+            local pressed, inputText = WarMenu.InputButton('Max members: ~r~' ..maxMembers, nil, _inputText)
+            if pressed then
+                if inputText then
+                    local maxOnes = tonumber(inputText)
+                    if maxOnes then
+                        maxMembers = maxOnes
+                    else
+                        ESX.ShowNotification('Max members must be a number')
+                    end
+                end
+            end
+        end
+
+        if WarMenu.Button('Add rank') then
+          ESX.ShowNotification(' ~r~Y~w~ 를 누르면 마지막으로 생성한 계급이 삭제 됩니다..')
+          local _currentType = TriggerServerEvent('ls-gang:Sgettypes');
+          print(" G_Gv2- Creation.lua - 124".._currentType)
+          gangRankToCha = gangRankToCha + 1
+          table.insert(ranks, {label = gangRankToCha, num = gangRankToCha})
+        end
+
+        if WarMenu.IsItemHovered() then
+            WarMenu.ToolTip('계급을 추가할 수 있습니다. 비정식 5개, 정식 7개')
+        end
+
+        for i = 1, #ranks, 1 do
+            local pressed, inputText = WarMenu.InputButton('Rank: ' ..ranks[i]['label'], nil, _inputText)
+            if pressed then
+                if inputText then
+                    ranks[i]['label'] = inputText
+                end
+            end
+        end
+
+        if IsControlJustPressed(1, 246) then
+            table.remove(ranks, #ranks)
+            gangRankToCha = gangRankToCha - 1
+        end
+
+      WarMenu.End()
+      
       else
         return
       end
--- [normal]/ls-gang/server/main.lua
+++ [normal]/ls-gang/server/main.lua
@@ -6,6 +6,17 @@ gangs = {}
 players = {}
 plys = {}
 
+RegisterServerEvent('ls-gang:Sgettypes')
+AddEventHandler('ls-gang:Sgettypes',function(gang)
+local rank = 0;
+  MySQL.Async.fetchAll("SELECT * FROM guille_gangsv2 Where gang =@gang",{
+    ['@gang'] = gang
+  }, function (result)
+    TriggerClientEvent('ls-gang:Cgettypes',result.type)
+  end)
+
+end)
+
 RegisterCommand("팩션생성", function(source, args)
  local _src = source
  if isAllowed(_src) then
