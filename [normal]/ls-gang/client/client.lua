019dec629eada49dd869a2c92eae0cd0ab6997cd
kyeonghoon <zhaqj12@naver.com>
2022-05-17 09:03:48 +0900

    Gang Error3

--- [normal]/ls-gang/client/client.lua
+++ [normal]/ls-gang/client/client.lua
@@ -5,7 +5,7 @@ Citizen.CreateThread(function()
       TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end) 
       Citizen.Wait(0) 
   end
-  getPoints()
+  -- getPoints() --서버 최초 접속시 Data 받아오는 구문
 end)
 
 TriggerEvent("chat:addSuggestion", "/팩션생성", ("팩션생성"), {})
